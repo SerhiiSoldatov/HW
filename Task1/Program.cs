2446cc25c699ce4182ed7664aefd283cec67c229
Serhii <serhii.soldatov@gmail.com>
2022-05-25 08:25:44 +0200

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,26 @@
 // M = 6; N = 2. -> "6, 5, 4, 3, 2"
 // M = 1; N = 5. -> "1, 2, 3, 4, 5"
 // M = 4; N = 8. -> "4, 6, 7, 8"
+
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+// if (M > N)
+//     for (int i = M; i >= N; i--)
+//         Console.Write($" {i}");
+// else
+//     for (int i = M; i <= N; i++)
+//         Console.Write($" {i}");
+
+void SomeRec(int n, int m)
+{
+    if (n < m)
+    SomeRec(n + 1, m);
+    if (n > m)
+    SomeRec(n - 1, m);
+    Console.Write(n + " ");
+
+}
+SomeRec(N, M);
