852b5014a4f2934702c0d8ad021bc28f799e11d6
Serhii <serhii.soldatov@gmail.com>
2022-05-25 08:48:46 +0200

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,20 @@
 // натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+int sum = 0;
+
+void SomeRec(int m, int n)
+{
+    for (int i = n; i <= m; i++)
+    {
+        sum += i;
+    }
+    return;
+    SomeRec(n, m);
+}
+SomeRec(N, M);
+Console.WriteLine($"Сумма чисел от M до N равна {sum}");
